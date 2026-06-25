1c93e2b8c6904f5c4be1886fe343cfcda1041a98
Kirti Gautam <kirtigautam110@gmail.com>
2026-06-25 13:30:19 +0530

    Added Singleton Pattern Example

---
+++ week1/singletonPattern.java
@@ -0,0 +1,37 @@
+public class singletonPattern {
+
+    static class Logger {
+
+        private static Logger instance;
+
+        private Logger() {
+            System.out.println("Logger Instance Created");
+        }
+
+        public static Logger getInstance() {
+            if (instance == null) {
+                instance = new Logger();
+            }
+            return instance;
+        }
+
+        public void log(String message) {
+            System.out.println("LOG: " + message);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+
+        logger1.log("Application Started");
+        logger2.log("Singleton Pattern Example");
+
+        if (logger1 == logger2) {
+            System.out.println("Only one Logger instance exists.");
+        } else {
+            System.out.println("Multiple Logger instances exist.");
+        }
+    }
+}
