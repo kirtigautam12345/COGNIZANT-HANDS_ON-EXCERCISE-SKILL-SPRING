9179c201a0f30c58712e191d584db513cbd3855f
Kirti Gautam <kirtigautam110@gmail.com>
2026-07-03 20:37:51 +0530

    JUnit & Mockito Exercises Commit

---
+++ Mockito exercises/Verifying Interactions/src/test/java/com/example/MyServiceTest.java
@@ -0,0 +1,19 @@
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.mock;
+
+import org.junit.jupiter.api.Test;
+
+public class MyServiceTest {
+
+    @Test
+    public void testVerifyInteraction() {
+
+        ExternalApi mockApi = mock(ExternalApi.class);
+
+        MyService service = new MyService(mockApi);
+
+        service.fetchData();
+
+        verify(mockApi).getData();
+    }
+}
