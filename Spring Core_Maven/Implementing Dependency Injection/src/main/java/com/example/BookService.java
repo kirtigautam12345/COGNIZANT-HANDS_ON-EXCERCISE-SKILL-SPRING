0ce887e933e66a438357787de27c2224d7e8f889
Kirti Gautam <kirtigautam110@gmail.com>
2026-07-04 12:54:06 +0530

    Dependency

---
+++ Spring Core_Maven/Implementing Dependency Injection/src/main/java/com/example/BookService.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class BookService {
+
+    private BookRepository bookRepository;
+
+    // Setter Injection
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void displayService() {
+        System.out.println("Book Service Called");
+        bookRepository.displayBook();
+    }
+}
