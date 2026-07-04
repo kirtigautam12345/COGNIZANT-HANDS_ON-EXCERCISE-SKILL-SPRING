9e99e8dab3536c814b4dbcb64c0bc570b5bd32c1
Kirti Gautam <kirtigautam110@gmail.com>
2026-07-04 16:28:59 +0530

    Spring Data JPA Commit

---
+++ spring-data-jpa-handson/Spring Data JPA Example/src/main/java/com/example/demo/service/CountryService.java
@@ -0,0 +1,23 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Country;
+import com.example.demo.repository.CountryRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class CountryService {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    @Transactional
+    public List<Country> getAllCountries() {
+        return countryRepository.findAll();
+    }
+}
