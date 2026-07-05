e373fe5d90f9309c65e69e38958267eb1c72d299
Kirti Gautam <kirtigautam110@gmail.com>
2026-07-05 21:19:52 +0530

    Load-Country

---
+++ spring-rest-handson/load-country/src/test/java/com/cognizant/load_country/LoadCountryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.load_country;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadCountryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
